9b10a0dbb0d43e5c62284c256de198da6a6054b2
Erik Barroso <coderik1996@gmail.com>
2024-03-15 13:16:30 -0300

    tamanho de array

--- 001/main.cpp
+++ 001/main.cpp
@@ -3,9 +3,12 @@ using namespace std;
 
 int main()
 {
-  // percorrer array
+  // tamanho da array
   double score[] = {8, 7, 10, 8.5};
-  for (int i = 0; i < 4; i++)
+
+  int sizeOfArray = sizeof(score) / sizeof(double) ;
+  
+  for (int i = 0; i < sizeOfArray; i++)
   {
     cout << "Score " << i + 1 << " --> " << score[i] << endl;
   }
