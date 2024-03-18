f827063e92d7a3f47631773beaaeedea878455f3
Erik Barroso <coderik1996@gmail.com>
2024-03-18 07:11:04 -0300

    alterando valor de variável com ponteiro

--- 001/main.cpp
+++ 001/main.cpp
@@ -4,12 +4,15 @@ using namespace std;
 int main()
 {
  int x = 10;
+int *pointer = &x;
 
-int *pointerX = &x;
+*pointer = 300;
+cout << "Valor de x alterado pelo ponteiro: " << x << endl; //300
+cout << "pointer: " << *pointer << endl;                    // 300
 
- cout << "Valor de X: " << x<< endl;
- cout << "Ponteiro de  X: " << pointerX << endl;
- cout << "Valor de x  pelo ponteiro de  X: " << *pointerX << endl;
+x = 100;
+cout << "Valor do ponteiro alterado por x: " << *pointer << endl;  //100
+cout << "x: " << x << endl;                                        // 100
 
- return 0;
+return 0;
 }
