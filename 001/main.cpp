839170bedbcfad222c1f0d6bfa756a36d598721b
Erik Barroso <coderik1996@gmail.com>
2024-03-18 06:35:47 -0300

    Ponteiro e desreferencia

--- 001/main.cpp
+++ 001/main.cpp
@@ -3,32 +3,13 @@ using namespace std;
 
 int main()
 {
- string name;
- double notas[4];
+ int x = 10;
 
- cout << "Digite seu nome ";
- cin >> name;
-cout << "Digite a nota do primeiro periodo: ";
-cin >> notas[0];
+int *pointerX = &x;
 
-cout << "Digite a nota do segundo periodo: ";
-cin >> notas[1];
+ cout << "Valor de X: " << x<< endl;
+ cout << "Ponteiro de  X: " << pointerX << endl;
+ cout << "Valor de x  pelo ponteiro de  X: " << *pointerX << endl;
 
-cout << "Digite a nota do terceiro periodo: ";
-cin >> notas[2];
-
-cout << "Digite a nota do quarto periodo: ";
-cin >> notas[3];
-
-double media;
-media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
-cout << "A media de  "<< name << " foi: " << media << endl;
-
-if(media >=7) {
-  cout << name << " passou de ano." << endl;
-} else {
-  cout << name << " perdeu de ano." << endl;
-}
-
-return 0;
+ return 0;
 }
