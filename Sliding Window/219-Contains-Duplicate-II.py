952968c417799956b9c03fa47b6d085945f3b9b9
Erik Barroso <117813798+ErikBarroso@users.noreply.github.com>
2025-04-04 18:24:04 -0300

    219-Contains-Duplicate-II.py

---
+++ Sliding Window/219-Contains-Duplicate-II.py
@@ -0,0 +1,17 @@
+def containsNearbyDuplicate(nums, k):
+    counter = {}
+
+    for index ,number in enumerate(nums):
+        if number in counter:
+            if index - counter[number] <= k:
+                return True
+        counter[number] = index
+    return False
+
+nums = [1,2,3,1,2,3]
+k = 2
+containsNearbyDuplicate(nums, k)
+
+# Você deve verificar se há dois números duplicados onde a distância entre seus índices é menor ou igual a 3.
+# Neste caso, nums[0] = 1 e nums[3] = 1, e a diferença entre seus índices é abs(0 - 3) = 3.
+# Como a diferença 3 é menor ou igual a k(3), a resposta é verdadeira.
