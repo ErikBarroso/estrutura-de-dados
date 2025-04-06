3678b73076208f48ef7eba75032c5098992c6b8b
Erik Barroso <117813798+ErikBarroso@users.noreply.github.com>
2025-04-06 20:13:12 -0300

    594. Maior Subsequência Harmoniosa

--- Sliding Window/594-Longest-Harmonious-Subsequence.py
+++ Sliding Window/594-Longest-Harmonious-Subsequence.py
@@ -1,19 +1,15 @@
 def findLHS(nums):
-    lef, right = 0, 0
     subSequence = {}
-    subSequence[nums[0]] = 1
     subSequenceSize = 0
 
-    while right < len(nums) -1:
-        right += 1
-        if subSequence.get(nums[right]):
-            subSequence[nums[right]] += 1
+    for number in nums:
+        if number in subSequence:
+            subSequence[number] += 1
         else:
-            subSequence[nums[right]] = 1
+            subSequence[number] = 1
     for number in subSequence:
-        print(len(subSequence))
-         if number+1 in subSequence and len(subSequence) < (subSequence[number] + subSequence[number+1]):
-            subSequenceSize = subSequence[number] + subSequence[number+1]
+        if number+1 in subSequence:
+            subSequenceSize = max(subSequenceSize, subSequence[number] + subSequence[number+1])
     print(subSequenceSize)
     return subSequenceSize
             
